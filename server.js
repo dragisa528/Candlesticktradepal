380dda697e4d58cf6ad024928b3efa874d1a1373
techking5287 <techking5287@gmail.com>
2023-09-13 17:32:52 +0900

    frontend

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ app.use(express.json());
 // })
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, '/frontend')));
 
 app.get('*', function (request, response) {
-    response.sendFile(path.resolve(__dirname + '/build', 'index.html'));
+    response.sendFile(path.resolve(__dirname + '/frontend', 'index.html'));
 });
 app.use("/api/users", User_router);
 app.listen(port, () => console.log("server started at 5000"));
