f0aed92a97e2bb7fac62020a49e348f6aacd7f86
Full Stack || Blockchain Eng (Techking5287) <91337512+Techking5287@users.noreply.github.com>
2023-09-27 03:09:37 +0900

    updated-https

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
+const https = require("https")
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 5000;
 const User_router = require('./routes/User-router')
 require('./config/config(db)');
 const cors = require('cors');
-
-
-
-
-
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -19,6 +16,11 @@ app.use(express.json());
 //     res.setHeader('X-Foo', 'bar')
 
 // })
+const options = {
+    key: fs.readFileSync('selfsigned.key', 'utf8'),
+    cert: fs.readFileSync('selfsigned.crt', 'utf8')
+}
+
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/frontend')));
@@ -27,4 +29,8 @@ app.get('*', function (request, response) {
     response.sendFile(path.resolve(__dirname + '/frontend', 'index.html'));
 });
 app.use("/api/users", User_router);
-app.listen(port, () => console.log("server started at 5000"));
+// var httpServer = http.createServer(app);
+var httpsServer = https.createServer(options, app);
+httpsServer.listen(port);
+// app.listen(port, () => console.log("server started at 5000"));
+// https.createServer(options, app).listen(port, console.log(`server runs on port ${port}`))
